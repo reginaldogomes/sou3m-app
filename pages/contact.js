bf442a1be363a2b0b7ca2dd1d8612762f8271d38
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2020-01-09 17:43:59 -0300

    page contact created

---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Contact = () => (
+    <Container>
+      <Heading>Fale Conosco</Heading>
+    </Container>
+)
+
+const Container = styled.div`
+  width: 960px;
+  height: 100vh;
+  margin: 2rem auto;
+  padding: 2rem;
+  background: #f2f2f2;
+`
+
+const Heading = styled.h1``
+
+export default Contact
