b804ae2cf83dd67b9ecfa600049158d5ed0b3ac4
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2020-03-06 13:52:46 -0300

    footer and nav fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,45 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import styled from 'styled-components'
-import Link from 'next/link'
 
 const Footer = () => (
-    <footer>
-        <Container>
-            copyright - sou 3M2020 - todos os direitos reservados
-        </Container>
-    </footer>
+    <FooterComponent>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Paper>xs=12</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Paper>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Paper>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <Paper>xs=6 sm=3</Paper>
+            </Grid>
+        </Grid>
+        <Typography component="p">
+            © copyright {new Date().getFullYear()} - SOU 3M - todos os direitos reservados
+        </Typography>
+    </FooterComponent>
 )
 
-const Container = styled.div`
-  background: $primary;
+const FooterComponent = styled.footer`
+  background: #cf7010;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
 `
 
 export default Footer
--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { label: '/home', href: 'https://zeit.co/now', icon: 'ZEIT' },
+  { label:'/contact', href: 'https://github.com/zeit/next.js', icon: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
@@ -15,6 +15,9 @@ const Nav = () => (
       <li>
         <Link href="/">
           <a>Home</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contato</a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
