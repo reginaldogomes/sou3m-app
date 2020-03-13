2166c40211691275ed13f7122607b3e117ed94e6
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2020-03-13 16:36:05 -0300

    page blog created

---
+++ pages/blog.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Blog = () => (
+    <Container>
+      <Heading>Blog</Heading>
+    </Container>
+)
+
+const Container = styled.div`
+  width: 960px;
+  height: 100vh;
+  margin: 2rem auto;
+  padding: 2rem;
+  background: #f2f2f2;
+`
+
+const Heading = styled.h1``
+
+export default Blog
